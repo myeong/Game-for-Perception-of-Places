dab783f6417e42466217fe7086c0740de6c5442c
deeper.myeong.lee <deeper.myeong.lee@gmail.com>
2014-01-15 23:34:06 -0500

    db info modified

--- db.php
+++ db.php
@@ -5,9 +5,16 @@ function connect() {
           or die('Could not connect: ' . mysql_error());
   mysql_select_db('a8068967_psymap') or die('Could not select database');
   */
-	$link = mysql_connect('localhost', 'root', 'root')
-	or die('Could not connect: ' . mysql_error());
-	mysql_select_db('a8068967_psymap') or die('Could not select database');
+	if (($_SERVER['SERVER_NAME']) == "localhost") {
+		$link = mysql_connect('localhost', 'root', 'root')
+		or die('Could not connect: ' . mysql_error());
+		mysql_select_db('a8068967_psymap') or die('Could not select database');
+	}
+	else {
+		$link = mysql_connect('localhost', 'myeong_drupal', 'drupal123')
+		or die('Could not connect: ' . mysql_error());
+		mysql_select_db('myeong_urban') or die('Could not select database');
+	}	
   return $link;
 }
 
