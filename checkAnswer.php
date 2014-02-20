581700e0175f8e1adc1896b0b3cf16c33fdb8908
Myeong Lee <deeper.myeong.lee@gmail.com>
2014-02-20 01:11:43 -0500

    checkAnswer corrected

--- checkAnswer.php
+++ checkAnswer.php
@@ -8,9 +8,9 @@ $t = $_GET['t'];
 if($t != 'familiarity') die('0');
 
 if (is_numeric($ans)){
-  $q = "SELECT * FROM address WHERE id='$ans'";
+  $q = "SELECT * FROM address WHERE tfl_id='$ans'";
 } else {
-  $q = "SELECT * FROM landmarks WHERE name='$ans'";
+  $q = "SELECT * FROM landmarks WHERE ons_label='$ans'";
 }
 
 $result = query($q);
